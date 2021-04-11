cf038d6394f8366a667b9d3126aa1c2e61063398
Redless <brookmangabriel@gmail.com>
2021-04-11 00:16:37 -0600

    removed process/manager since no longer needed. use redis if u want

--- app.py
+++ app.py
@@ -1,37 +1,35 @@
 from flask import Flask, jsonify, request
-from multiprocessing import Manager, Process
 from flask_cors import CORS
 
 app = Flask(__name__, static_folder='app', static_url_path="/app")
 
 CORS(app)
 
-leader = Manager()
-p1moves = leader.list()
-p2moves = leader.list()
-movereceived = leader.list()
-movereceived.append(False)
-movereceived.append(False)
-score = leader.list()
-score.append(0)
-score.append(0)
+p1movereceived=False
+p2movereceived=False
+p2moves = []
+p1moves = []
+p1score = 0
+p2score = 0
 
 rpsdict = {"rock":0,"paper":1,"scissors":2}
 
 def updatescore():
+    global p1score
+    global p2score
     if len(p1moves) != len(p2moves):
         1/0
     p1move = rpsdict[p1moves[-1]]
     p2move = rpsdict[p2moves[-1]]
-    movereceived[0]=False
-    movereceived[1]=False
+    p2movereceived=False
+    p1movereceived=False
     if p1move == p2move:
         return
     if ((p1move+1)%3) == p2move:
-        score[1]+=1
+        p2score+=1
         return
     if ((p2move+1)%3) == p1move:
-        score[0]+=1
+        p1score+=1
         return
     1/0
 
@@ -42,29 +40,28 @@ def result():
     received = request.get_json(force=True)
     side = received["side"]
     move = received["move"]
-    p = Process(target=process_new_move, args=(side,move))
-    p.start()
-    p.join()
-    return ": )"
+    process_new_move(side,move)
+    return "{}"
 
 def process_new_move(side,move):
+    global p1movereceived
+    global p2movereceived
     if not side:
-        if not movereceived[side]:
-            movereceived[side] = True
+        if not p1movereceived:
+            p1movereceived = True
             p1moves.append(move)
 
     else:
-        if not movereceived[side]:
-            movereceived[side] = True
+        if not p2movereceived:
+            p2movereceived = True
             p2moves.append(move)
-    if movereceived[0] and movereceived[1]:
+    if p2movereceived and p1movereceived:
         updatescore()
 
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
-    response = jsonify({"p1score":score[0],"p2score":score[1],"p1moves":list(p1moves),"p2moves":list(p2moves)})
-    print(response)
+    response = jsonify({"p1score":p1score,"p2score":p2score,"p1moves":p1moves,"p2moves":p2moves})
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
